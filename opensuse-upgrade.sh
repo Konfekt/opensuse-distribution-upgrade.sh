bb2b34f0660db4e473a50d6308b4c2c346f84c09
Konfekt <Konfekt@users.noreply.github.com>
2020-07-19 14:54:59 +0200

    do not install recommended packages by default

--- opensuse-upgrade.sh
+++ opensuse-upgrade.sh
@@ -24,7 +24,7 @@ new="$1"
 
 z() { sudo zypper --non-interactive --ignore-unknown --no-gpg-checks --gpg-auto-import-keys "$@"; }
 # download all required packages in advance and install afterwards
-params="--download in-advance --recommends --auto-agree-with-licenses --replacefiles --force-resolution --no-confirm"
+params="--download in-advance --auto-agree-with-licenses --replacefiles --force-resolution --no-confirm"
 
 echo "Updating current version $old ..."
 z --releasever "$old" refresh --force
@@ -38,16 +38,16 @@ sudo sed -i "s/${old//./\\.}/${new}/g" /etc/zypp/repos.d/*.repo
 
 echo "Upgrading from version $old to version $new ..."
 z --releasever "$new" refresh --force
-z --releasever "$new" patch $params --updatestack-only
-z --releasever "$new" dist-upgrade $params 
+z --releasever "$new" patch --no-recommends $params --updatestack-only
+z --releasever "$new" dist-upgrade --no-recommends $params 
 
 if ! [ "$2" = "--no-packman" ]; then
   echo "Ensuring installation of media codes of version $new ..."
   # as listed on https://opensuse-guide.org/
   z addrepo --refresh "http://packman.inode.at/suse/openSUSE_Leap_$new/" packman
-  z --releasever "$new" install $params --allow-vendor-change \
+  z --releasever "$new" install --recommends $params --allow-vendor-change \
     ffmpeg lame gstreamer-plugins-bad gstreamer-plugins-ugly gstreamer-plugins-ugly-orig-addon gstreamer-plugins-libav libavdevice56 libavdevice58 vlc-codecs
-  z --releasever "$new" install $params \
+  z --releasever "$new" install --recommends $params \
     vlc plasma-browser-integration freshplayerplugin xine-browser-plugin java-1_8_0-openjdk-plugin
   z --releasever "$new" dist-upgrade --from packman --allow-vendor-change $params
 fi
